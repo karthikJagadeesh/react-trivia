b82968712373f87bbfce50978e74869af23d64a7
Karthik Jagadeesha <kartu8@gmail.com>
2019-03-30 16:59:52 +0530

    Add select difficulty dropdown

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import { capitalize } from "./utils";
+
+function Difficulty({ difficulty, setDifficulty }) {
+  const difficulties = ["easy", "medium", "hard"];
+
+  return (
+    <TextField
+      select
+      label="Difficulty"
+      value={difficulty}
+      onChange={({ target: { value } }) => setDifficulty(value)}
+      helperText="Select game difficulty"
+      margin="normal"
+      variant="outlined"
+    >
+      {difficulties.map(difficulty => (
+        <MenuItem key={difficulty} value={difficulty}>
+          {capitalize(difficulty)}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+}
 
 function App() {
-  return <div>Hello</div>;
+  const [difficulty, setDifficulty] = useState("easy");
+
+  return (
+    <div>
+      <Difficulty difficulty={difficulty} setDifficulty={setDifficulty} />
+    </div>
+  );
 }
 
 export default App;
